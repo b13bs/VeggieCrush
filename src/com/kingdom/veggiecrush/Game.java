e939ad3e7bd594c01d1dd558393cf1b83520325a
Laurence Lafontaine <laurence.lafontaine@gmail.com>
2014-02-05 17:29:54 -0500

    Ajout de belle image et logique de changement de texte

--- src/com/kingdom/veggiecrush/Game.java
+++ src/com/kingdom/veggiecrush/Game.java
@@ -1,21 +1,22 @@
 package com.kingdom.veggiecrush;
 
+import com.kingdom.veggiecrush.R.string;
+
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 
 public class Game extends Activity implements OnClickListener
 {
 
+	private Settings.GAME_MODE mode = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,6 +26,24 @@ public class Game extends Activity implements OnClickListener
 		btnExitGame.setOnClickListener(this);
 		Button btnNewGame = (Button) findViewById(R.id.btnNewGame);
 		btnNewGame.setOnClickListener(this);
+		
+		mode = (Settings.GAME_MODE) getIntent().getExtras().get(Settings.EXTRA_GAME_MODE);
+		TextView txtMode = (TextView) findViewById(R.id.TxtMode);
+		if(mode == Settings.GAME_MODE.TIME_ATTACK)
+			txtMode.setText(string.time_Attack);
+		else
+			txtMode.setText(string.blitz);
+		
+		TextView txtChain = (TextView) findViewById(R.id.txtChain);
+		txtChain.setText("0");
+	
+		TextView txtScore = (TextView) findViewById(R.id.txtScore);
+		txtScore.setText("0");
+		
+		TextView txtCountdown = (TextView) findViewById(R.id.txtCountdown);
+		txtCountdown.setText("0");
+		
+		playGame();
 	}
 	
 	@Override
@@ -41,7 +60,29 @@ public class Game extends Activity implements OnClickListener
 		}
 	}
 	
-	public void promptExit()
+	@Override
+	public void onBackPressed()
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Exit the game").setMessage("Do you really want to quit the game?");
+		builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				finish();
+			}
+		});
+		builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+	    builder.show();	
+	}
+	
+	private void promptExit()
 	{
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("Exit the game").setMessage("Do you really want to quit the game?");
@@ -64,5 +105,10 @@ public class Game extends Activity implements OnClickListener
 			}
 		});
 	    builder.show();	
+	}
+	
+	private void playGame()
+	{
+		//TODO: Implement ze game
 	}
 }
