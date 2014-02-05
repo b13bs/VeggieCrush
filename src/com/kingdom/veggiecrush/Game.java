4121d1080179369cfb00b2cbfd155452f5f7af99
Laurence Lafontaine <laurence.lafontaine@gmail.com>
2014-02-04 22:56:32 -0500

    #Pas Fini je vais travailler la dessus demain dans la journée

--- src/com/kingdom/veggiecrush/Game.java
+++ src/com/kingdom/veggiecrush/Game.java
@@ -1,14 +1,68 @@
 package com.kingdom.veggiecrush;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 
-public class Game extends Activity {
+public class Game extends Activity implements OnClickListener
+{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+		
+		Button btnExitGame = (Button) findViewById(R.id.btnExitGame);
+		btnExitGame.setOnClickListener(this);
+		Button btnNewGame = (Button) findViewById(R.id.btnNewGame);
+		btnNewGame.setOnClickListener(this);
 	}
-
+	
+	@Override
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
+			case R.id.btnExitGame:
+				promptExit();
+				break;
+			case R.id.btnNewGame:
+				//TODO : Refresh the gameplay
+				break;
+		}
+	}
+	
+	public void promptExit()
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Exit the game").setMessage("Do you really want to quit the game?");
+		builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Intent intent = new Intent(Intent.ACTION_MAIN);
+				intent.addCategory(Intent.CATEGORY_HOME);
+				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(intent);
+				
+			}
+		});
+		builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+	    builder.show();	
+	}
 }
